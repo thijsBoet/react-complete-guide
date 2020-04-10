42b54efd46d444231e67cf1272419b5013a3df6c
Matthijs Boet <m.boet@chello.nl>
2020-04-10 21:11:27 +0200

    added two way binding

--- 3-base-features/1-create-react-app/src/App.js
+++ 3-base-features/1-create-react-app/src/App.js
@@ -1,54 +1,66 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 // Use Capitalized name, lowercase is reserved for JSX HTML syntax
 import Person from "./Person/Person";
 
-const App = props => {
-  const [ personsState, setPersonsState ] = useState({
+class App extends Component {
+  state = {
       persons: [
         { name: "Matthijs", age: "38" },
         { name: "Max", age: "28" },
         { name: "Manu", age: "29" },
     ],
     otherState : 'Some other value'
-  });
+  };
 
-  const switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // DONT DO THIS: this.state.persons[0].name == 'Maximus Orilius' USE setState
     // setState Takes an object as an argument
-    setPersonsState({ 
+    this.setState({ 
       persons: [
-        { name: "Matthijs Boet", age: "38" },
+        { name: newName, age: "38" },
         { name: "Maximus Aorilius", age: "28" },
         { name: "Manu Manu", age: "29" },
       ],
     })
   };
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a real React App</h1>
-      <p>This is really working</p>
-      {/* Components are reusable and configurable */}
-      <button onClick={switchNameHandler}>Switch Names</button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      >
-        My Hobbies: Guitar
-      </Person>
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-      />
-      <Person
-        name={personsState.persons[2].name}
-        age={personsState.persons[2].age}
-      />
-    </div>
-  );
+  nameChangedHandler = (event) => {
+    this.setState({ 
+      persons: [
+        { name: "Darth Vader", age: "38" },
+        { name: event.target.value, age: "28" },
+        { name: "Manu Manu", age: "29" },
+      ],
+    })
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a real React App</h1>
+        <p>This is really working</p>
+        {/* Components are reusable and configurable */}
+        <button onClick={() => this.switchNameHandler('Pika')}>Switch Names</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          My Hobbies: Guitar
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
   // Code Above compiles to this React.createElement syntax
   // return React.createElement(
   //   "div",
--- 3-base-features/1-create-react-app/src/Person/Person.js
+++ 3-base-features/1-create-react-app/src/Person/Person.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 
-class Person extends Component{
-  render(){
-    return (
-      <div>
-        <p>I'm a {this.props.name} and I am {this.props.age} years old!</p>
-        <p>{this.props.children}</p>
-      </div>
-    )
-  }
+const Person = (props) => {
+  return (
+    <div>
+      <p>I'm {props.name} and I am {props.age} years old</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
+    </div>
+  )
 }
 
 export default Person;
