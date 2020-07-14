ce3645ea7fea1dba6139d53874180469e39ba40b
Matthijs Boet <m.boet@chello.nl>
2020-07-14 21:34:37 +0200

    moved back to hooks in section 3 to freshup on basics

--- 3-base-features/2-base-syntax-assignment/src/App.js
+++ 3-base-features/2-base-syntax-assignment/src/App.js
@@ -1,41 +1,78 @@
-import React, { Component } from 'react';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    username: "Matthijs Boet"
-  }
+const app = props => {
+  // asssigned to destructured variables
+  // this.state is now personState
+  // setPersonState lets use change state
+  const [ personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  });
 
-  nameChangedHandler = (e) => {
-    this.setState({
-      username: e.target.value
-    }) 
-  }
+  const switchNameHandler = () => {
+  // Instead this.setState use the destructured SetPersonState
+    setPersonsState({
+      persons: [
+        { name: "Maximilian", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 },
+      ],
+    });
+  };
+  // SetPersonState deletes/replaces old state, make sure to copy all future necessary STATE data
+  const [ otherState, setOtherState ] = useState('some other value');
 
-  render() {
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <ol>
-            <UserInput changed={this.nameChangedHandler} currentName={this.state.username}/>
-            <UserOutput username={this.state.username}/>
-            <li>Create TWO new components: UserInput and UserOutput</li>
-            <li>UserInput should hold an input element, UserOutput two paragraphs</li>
-            <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
-            <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
-            <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
-            <li>Add a method to manipulate the state (=> an event-handler method)</li>
-            <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
-            <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-            <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-            <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
-          </ol>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person
+          name={personsState.persons[0].name}
+          age={personsState.persons[0].age}
+        />
+        <Person
+          name={personsState.persons[1].name}
+          age={personsState.persons[1].age}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={personsState.persons[2].name}
+          age={personsState.persons[2].age}
+        />
       </div>
     );
-  }
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+
+  // state = {
+  //   persons: [
+  //     { name: 'Max', age: 28 },
+  //     { name: 'Manu', age: 29 },
+  //     { name: 'Stephanie', age: 26 }
+  //   ],
+  //   otherState: 'some other value'
+  // };
+
+  // switchNameHandler = () => {
+  //   // console.log('Was clicked!');
+  //   // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  //   this.setState({
+  //     persons: [
+  //       { name: 'Maximilian', age: 28 },
+  //       { name: 'Manu', age: 29 },
+  //       { name: 'Stephanie', age: 27 }
+  //     ]
+  //   });
+  // };
 }
 
-export default App;
+export default app;
