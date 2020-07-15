40ef9f9099cbedcac357e4718619588f6da3f6e1
Matthijs Boet <m.boet@chello.nl>
2020-07-15 05:39:49 +0200

    finished theory on 4-lists-conditionals

--- 4-lists-conditionals/1-lists/src/App.js
+++ 4-lists-conditionals/1-lists/src/App.js
@@ -1,82 +1,102 @@
-import React, { Component } from "react";
-import "./App.css";
-
-import Person from "./Person/Person";
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
     persons: [
-      { id: 1, name: "Matthijs", age: "38" },
-      { id: 2, name: "Max", age: "28" },
-      { id: 3, name: "Manu", age: "29" },
+      { id: 1, name: 'Max', age: 28 },
+      { id: 2, name: 'Manu', age: 29 },
+      { id: 3, name: 'Stephanie', age: 26 }
     ],
-    showPersons: false,
-  };
+    otherState: 'some other value',
+    showPersons: false
+  }
 
-  nameChangedHandler = (event, id) => {
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    // deletes one person at the personIndex
+    persons.splice(personIndex, 1);
+    // Bad practice to directly change reference types 
+    // Always first make a copy with spread operator ... 
+    // when changing values of reference types
+    // [...this.state.persons]
+    this.setState({
+      persons
+    })
+  }
+
+  nameChangedHandler = ( event, id ) => {
     const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id
+      return p.id === id;
     });
 
+    // Bad practice to directly change reference types 
+    // Always first make a copy with spread operator ... 
+    // when changing values of reference types
     const person = {
       ...this.state.persons[personIndex]
-    };
+    }
 
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
-    persons[personIndex] = person
+    persons[personIndex] = person;
 
-    this.setState({ persons : persons});
-  };
-
-  deletePersonHandler = (personIndex) => {
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons})
+    this.setState({persons})
   }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  };
+    this.setState({
+      showPersons: !doesShow
+    });
+  }
 
-  render() {
+  render () {
     const style = {
-      backgroundColor: "white",
-      font: "inherit",
-      border: "1px border blue",
-      padding: "8px",
-      cursor: "pointer",
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
     };
 
     let persons = null;
 
-    if (this.state.showPersons) {
+    if(this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)} 
-              name={person.name} 
-              age={person.age} 
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)}/>;
-          })}
-        </div>
+        <main>
+          {
+            this.state.persons.map((person, index) => {
+              return (
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+                  key={person.id}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}
+                >
+                  My Hobbies: Racing
+                </Person>
+              );
+            }
+          )}
+        </main>
       );
     }
 
     return (
       <div className="App">
-        <h1>Hi, I'm a real React App</h1>
-        <p>This is really working</p>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
         <button style={style} onClick={this.togglePersonsHandler}>
-          Show Persons
+          Toggle Persons
         </button>
-        {persons}
+          {persons}
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
--- 4-lists-conditionals/1-lists/src/Person/Person.js
+++ 4-lists-conditionals/1-lists/src/Person/Person.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
+import React from 'react';
+
 import './Person.css';
 
-const Person = (props) => {
-  return (
-    <div className="Person">
-      <p>I'm {props.name} and I am {props.age} years old</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-  )
-}
+const person = ( props ) => {
+    return (
+        <article className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </article>
+    )
+};
 
-export default Person;
+export default person;
